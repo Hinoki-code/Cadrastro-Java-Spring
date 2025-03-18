c4694a946ecd8cda9f351ccee77eefb227b46314
Iago Nascimento <iagonascimentosilva40@gmail.com>
2025-03-18 20:20:28 -0300

    primeiro commit

---
+++ src/main/java/Cadrastro_Java_Spring/CadrastroJavaSpringApplication.java
@@ -0,0 +1,13 @@
+package Cadrastro_Java_Spring;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CadrastroJavaSpringApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CadrastroJavaSpringApplication.class, args);
+	}
+
+}
---
+++ src/test/java/Cadrastro_Java_Spring/CadrastroJavaSpringApplicationTests.java
@@ -0,0 +1,13 @@
+package Cadrastro_Java_Spring;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CadrastroJavaSpringApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
